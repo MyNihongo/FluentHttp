09bfa7c75ba1ca7545bac5931dc36c1d3508c444
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-01-08 17:00:59 +0200

    move tests

--- tests/MyNihongo.HttpService.Tests.Unit/HttpServiceTests/HttpServiceTestsBase.cs
+++ tests/MyNihongo.HttpService.Tests.Unit/HttpServiceTests/HttpServiceTestsBase.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System.Threading;
+using Moq;
 
 namespace MyNihongo.HttpService.Tests.Unit.HttpServiceTests;
 
@@ -8,4 +9,12 @@ public abstract class HttpServiceTestsBase
 
 	protected IHttpService CreateFixture() =>
 		MockHttpService.Object;
+
+	internal void VerifyPost(RequestRecord req, HttpCallOptions options, CancellationToken ct)
+	{
+		MockHttpService
+			.Verify(x => x.PostJsonAsync<RequestRecord, ResponseRecord>(req, ItIs.Equivalent(options), null, null, ct), Times.Once);
+
+		MockHttpService.VerifyNoOtherCalls();
+	}
 }
