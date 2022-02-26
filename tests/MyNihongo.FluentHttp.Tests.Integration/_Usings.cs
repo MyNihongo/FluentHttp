168688c19f08dacfcb938d64cfd29ca19513ed17
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2022-02-26 15:04:49 +0200

    remove commens for ApprovalTests

--- tests/MyNihongo.FluentHttp.Tests.Integration/_Usings.cs
+++ tests/MyNihongo.FluentHttp.Tests.Integration/_Usings.cs
@@ -1,6 +1,3 @@
 ﻿global using MyNihongo.FluentHttp.Tests.Integration.Models;
 global using Serilog.Events;
 global using Xunit;
-
-//[assembly: ApprovalTests.Reporters.UseReporter(typeof(ApprovalTests.Reporters.QuietReporter))]
-//[assembly: ApprovalTests.Namers.UseApprovalSubdirectory("Approvals")] 
