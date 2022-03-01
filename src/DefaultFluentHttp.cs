4d952e5ad29fda4539232dc24a04749505a6b2c7
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2022-03-01 22:07:51 +0200

    log the request time

--- src/DefaultFluentHttp.cs
+++ src/DefaultFluentHttp.cs
@@ -100,8 +100,12 @@ internal sealed class DefaultFluentHttp : IFluentHttp
 		var httpClient = _factory.CreateClient(Const.FactoryName);
 		var url = httpClient.BaseAddress.GetAbsoluteUri(req.RequestUri);
 
+		var startTime = DateTime.Now;
+
 		using var res = await httpClient.SendAsync(req, ct)
 			.ConfigureAwait(false);
+
+		_logger.LogDebug("Request time: {RequestTime}", DateTime.Now - startTime);
 
 		await using var stream = await res.Content
 			.ReadAsStreamAsync(ct)
