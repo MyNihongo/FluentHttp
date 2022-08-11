01fe517450355b98f3b65bd6afe7c74d18d08744
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-08-11 13:49:41 +0900

    create a URI with a base address

--- src/Models/HttpCallOptions.cs
+++ src/Models/HttpCallOptions.cs
@@ -2,6 +2,8 @@
 
 public sealed record HttpCallOptions
 {
+	public string BaseUrl { get; set; } = string.Empty;
+
 	public List<string> PathSegments { get; } = new();
 
 	public Dictionary<string, string> Headers { get; } = new();
--- src/Utils/Extensions/HttpCallOptionsEx.cs
+++ src/Utils/Extensions/HttpCallOptionsEx.cs
@@ -2,6 +2,10 @@
 
 internal static class HttpCallOptionsEx
 {
-	public static Uri CreateUri(this HttpCallOptions @this) =>
-		new(@this.PathSegments.Join(Const.UriSeparator), UriKind.Relative);
+	public static Uri CreateUri(this HttpCallOptions @this)
+	{
+		return !string.IsNullOrEmpty(@this.BaseUrl)
+			? new Uri(@this.BaseUrl.Join(@this.PathSegments, Const.UriSeparator), UriKind.Absolute)
+			: new Uri(@this.PathSegments.Join(Const.UriSeparator), UriKind.Relative);
+	}
 }
