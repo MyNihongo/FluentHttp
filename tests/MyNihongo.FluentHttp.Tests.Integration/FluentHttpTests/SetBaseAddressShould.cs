5f319a8d31308602e6505c1f0baf501306c4ac10
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-08-11 14:06:54 +0900

    add integration tests

---
+++ tests/MyNihongo.FluentHttp.Tests.Integration/FluentHttpTests/SetBaseAddressShould.cs
@@ -0,0 +1,41 @@
+﻿namespace MyNihongo.FluentHttp.Tests.Integration.FluentHttpTests;
+
+[UsesVerify]
+public sealed class SetBaseAddressShould : FluentHttpTestsBase
+{
+	public SetBaseAddressShould()
+		: base(false)
+	{
+	}
+
+	[Fact]
+	public async Task GetFromBaseUri()
+	{
+		var options = new HttpCallOptions
+		{
+			BaseAddress = "https://jsonplaceholder.typicode.com/users/1"
+		};
+
+		var result = await CreateFixture()
+			.GetJsonAsync(options, UserRecordContext.Default.UserRecord)
+			.ToJsonStringAsync();
+
+		await Verify(result);
+	}
+
+	[Fact]
+	public async Task GetFromBaseUriAndPaths()
+	{
+		var options = new HttpCallOptions
+		{
+			BaseAddress = "https://jsonplaceholder.typicode.com",
+			PathSegments = { "users", "1" }
+		};
+
+		var result = await CreateFixture()
+			.GetJsonAsync(options, UserRecordContext.Default.UserRecord)
+			.ToJsonStringAsync();
+
+		await Verify(result);
+	}
+}
