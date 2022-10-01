9613790bc6eee917ddb1749f0a97ef33649e6a7e
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-10-01 20:52:48 +0900

    append the params

--- src/Utils/Extensions/HttpCallOptionsEx.cs
+++ src/Utils/Extensions/HttpCallOptionsEx.cs
@@ -5,19 +5,35 @@ internal static class HttpCallOptionsEx
 	public static Uri CreateUri(this HttpCallOptions @this)
 	{
 		UriKind uriKind;
-		string uriString;
+		StringBuilder uriStringBuilder;
 
 		if (string.IsNullOrEmpty(@this.BaseAddress))
 		{
-			uriString = @this.PathSegments.Join(Const.UriSeparator);
+			uriStringBuilder = @this.PathSegments.Join(Const.UriSeparator);
 			uriKind = UriKind.Relative;
 		}
 		else
 		{
-			uriString = @this.BaseAddress.Join(@this.PathSegments, Const.UriSeparator);
+			uriStringBuilder = @this.BaseAddress.Join(@this.PathSegments, Const.UriSeparator);
 			uriKind = UriKind.Absolute;
 		}
 
-		return new Uri(uriString, uriKind);
+		if (@this.Parameters.Count != 0)
+		{
+			uriStringBuilder
+				.Append('?');
+
+			var i = 0;
+			foreach (var (key, value) in @this.Parameters)
+			{
+				if (i != 0)
+					uriStringBuilder.Append('&');
+
+				uriStringBuilder.Append(key).Append('=').Append(value);
+				i++;
+			}
+		}
+
+		return new Uri(uriStringBuilder.ToString(), uriKind);
 	}
 }
--- tests/MyNihongo.FluentHttp.Tests.Unit/Utils/HttpCallOptionsExTests/CreateUriShould.cs
+++ tests/MyNihongo.FluentHttp.Tests.Unit/Utils/HttpCallOptionsExTests/CreateUriShould.cs
@@ -49,7 +49,97 @@ public sealed class CreateUriShould
 	}
 
 	[Fact]
-	public void ReturnRelativePathWithoutSegments()
+	public void ReturnAbsoluteUriWithParameter()
+	{
+		const string baseAddress = "https://mynihongo.org/tracks",
+			key = nameof(key), value = nameof(value);
+
+		const string expected = $"{baseAddress}?{key}={value}";
+
+		var fixture = new HttpCallOptions
+		{
+			BaseAddress = baseAddress,
+			Parameters =
+			{
+				{ key, value }
+			}
+		};
+
+		var result = fixture.CreateUri();
+
+		result.AbsoluteUri
+			.Should()
+			.Be(expected);
+
+		result.IsAbsoluteUri
+			.Should()
+			.BeTrue();
+	}
+
+	[Fact]
+	public void ReturnAbsoluteUriWithParameters()
+	{
+		const string baseAddress = "https://mynihongo.org/tracks",
+			key1 = nameof(key1), value1 = nameof(value1),
+			key2 = nameof(key2), value2 = nameof(value2);
+
+		const string expected = $"{baseAddress}?{key1}={value1}&{key2}={value2}";
+
+		var fixture = new HttpCallOptions
+		{
+			BaseAddress = baseAddress,
+			Parameters =
+			{
+				{ key1, value1 },
+				{ key2, value2 }
+			}
+		};
+
+		var result = fixture.CreateUri();
+
+		result.AbsoluteUri
+			.Should()
+			.Be(expected);
+
+		result.IsAbsoluteUri
+			.Should()
+			.BeTrue();
+	}
+
+	[Fact]
+	public void ReturnAbsoluteUriWithSegmentsAndParameters()
+	{
+		const string baseAddress = "https://mynihongo.org/tracks",
+			segment1 = nameof(segment1), segment2 = nameof(segment2),
+			key1 = nameof(key1), value1 = nameof(value1),
+			key2 = nameof(key2), value2 = nameof(value2);
+
+		const string expected = $"{baseAddress}/{segment1}/{segment2}?{key1}={value1}&{key2}={value2}";
+
+		var fixture = new HttpCallOptions
+		{
+			BaseAddress = baseAddress,
+			PathSegments = { segment1, segment2 },
+			Parameters =
+			{
+				{ key1, value1 },
+				{ key2, value2 }
+			}
+		};
+
+		var result = fixture.CreateUri();
+
+		result.AbsoluteUri
+			.Should()
+			.Be(expected);
+
+		result.IsAbsoluteUri
+			.Should()
+			.BeTrue();
+	}
+
+	[Fact]
+	public void ReturnRelativePath()
 	{
 		const string segment1 = nameof(segment1), segment2 = nameof(segment2);
 
@@ -58,6 +148,64 @@ public sealed class CreateUriShould
 		var fixture = new HttpCallOptions
 		{
 			PathSegments = { segment1, segment2 }
+		};
+
+		var result = fixture.CreateUri();
+
+		result.OriginalString
+			.Should()
+			.Be(expected);
+
+		result.IsAbsoluteUri
+			.Should()
+			.BeFalse();
+	}
+
+	[Fact]
+	public void ReturnRelativePathWithParameter()
+	{
+		const string segment1 = nameof(segment1), segment2 = nameof(segment2),
+			key = nameof(key), value = nameof(value);
+
+		const string expected = $"{segment1}/{segment2}?{key}={value}";
+
+		var fixture = new HttpCallOptions
+		{
+			PathSegments = { segment1, segment2 },
+			Parameters =
+			{
+				{ key, value }
+			}
+		};
+
+		var result = fixture.CreateUri();
+
+		result.OriginalString
+			.Should()
+			.Be(expected);
+
+		result.IsAbsoluteUri
+			.Should()
+			.BeFalse();
+	}
+
+	[Fact]
+	public void ReturnRelativePathWithParameters()
+	{
+		const string segment1 = nameof(segment1), segment2 = nameof(segment2),
+			key1 = nameof(key1), value1 = nameof(value1),
+			key2 = nameof(key2), value2 = nameof(value2);
+
+		const string expected = $"{segment1}/{segment2}?{key1}={value1}&{key2}={value2}";
+
+		var fixture = new HttpCallOptions
+		{
+			PathSegments = { segment1, segment2 },
+			Parameters =
+			{
+				{ key1, value1 },
+				{ key2, value2 }
+			}
 		};
 
 		var result = fixture.CreateUri();
