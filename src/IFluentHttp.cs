72692210fb7588e9df7a11e657bcd2236214f374
kuzmins-85 <kuzmins-85@soft-gear.co.jp>
2022-11-28 10:38:40 +0900

    add methods for JsonSerializerOptions

--- src/IFluentHttp.cs
+++ src/IFluentHttp.cs
@@ -23,4 +23,24 @@ public interface IFluentHttp
 	/// </summary>
 	/// <exception cref="HttpCallException"></exception>
 	Task<TResult?> PostJsonOrDefaultAsync<TSource, TResult>(TSource source, HttpCallOptions options, JsonTypeInfo<TSource>? sourceTypeInfo = null, JsonTypeInfo<TResult>? resultTypeInfo = null, CancellationToken ct = default);
+	
+	/// <exception cref="HttpCallException"></exception>
+	/// <exception cref="JsonException"></exception>
+	Task<TResult> GetJsonAsync<TResult>(HttpCallOptions options, JsonSerializerOptions jsonOptions, CancellationToken ct = default);
+	
+	/// <summary>
+	///	Attempts to GET a JSON value. Returns <see langword="null"/> in case the returned model is not a valid JSON.
+	/// </summary>
+	/// <exception cref="HttpCallException"></exception>
+	Task<TResult?> GetJsonOrDefaultAsync<TResult>(HttpCallOptions options, JsonSerializerOptions jsonOptions, CancellationToken ct = default);
+	
+	/// <exception cref="HttpCallException"></exception>
+	/// <exception cref="JsonException"></exception>
+	Task<TResult> PostJsonAsync<TSource, TResult>(TSource source, HttpCallOptions options, JsonSerializerOptions jsonOptions, CancellationToken ct = default);
+
+	/// <summary>
+	///	Attempts to POST a JSON value. Returns <see langword="null"/> in case the returned model is not a valid JSON.
+	/// </summary>
+	/// <exception cref="HttpCallException"></exception>
+	Task<TResult?> PostJsonOrDefaultAsync<TSource, TResult>(TSource source, HttpCallOptions options, JsonSerializerOptions jsonOptions, CancellationToken ct = default);
 }
