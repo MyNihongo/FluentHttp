0cf9810b50243f68b1cc965c664dfcbcf04a0b48
kuzmins-85 <kuzmins-85@soft-gear.co.jp>
2022-11-30 10:58:58 +0900

    add a sample console app to run http requests

---
+++ sample/MyNihongo.FluentHttp.Sample.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyNihongo.FluentHttp;
+using Serilog;
+
+var currentDirectory = AppContext.BaseDirectory;
+Directory.SetCurrentDirectory(currentDirectory);
+
+var configuration = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json")
+	.Build();
+
+var serilogLogger = new LoggerConfiguration()
+	.Enrich.FromLogContext()
+	.MinimumLevel.Verbose()
+	.WriteTo.Console()
+	.CreateLogger();
+
+var services = new ServiceCollection()
+	.AddSingleton<IConfiguration>(configuration)
+	.AddLogging(x => x.AddSerilog(serilogLogger))
+	.AddFluentHttp()
+	.BuildServiceProvider(true);
+
+var fluentHttp = services.GetRequiredService<IFluentHttp>();
+
+// GET 200
+
+// POST 200
+
+// POST non-200
