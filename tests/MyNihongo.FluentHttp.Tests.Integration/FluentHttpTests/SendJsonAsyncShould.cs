53f40d8f4fe60c2c3aec8ee6abd4b976e1bc9256
kuzmins-85 <kuzmins-85@soft-gear.co.jp>
2023-01-17 12:46:24 +0900

    add tests for send

---
+++ tests/MyNihongo.FluentHttp.Tests.Integration/FluentHttpTests/SendJsonAsyncShould.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.Json;
+
+namespace MyNihongo.FluentHttp.Tests.Integration.FluentHttpTests;
+
+[UsesVerify]
+public sealed class SendJsonAsyncShould : FluentHttpTestsBase
+{
+	public SendJsonAsyncShould(ITestOutputHelper testOutputHelper)
+		: base(testOutputHelper)
+	{
+	}
+
+	[Fact]
+	public async Task SendGetWithOptions()
+	{
+		var jsonOptions = new JsonSerializerOptions
+		{
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+		};
+		
+		var result = await CreateFixture()
+			.AppendPathSegment("comments")
+			.AppendParameter("postId", 1)
+			.SendJsonAsync<PostCommentRecord[]>(HttpMethod.Get, null, jsonOptions)
+			.ToJsonStringAsync();
+		
+		await Verify(result);
+	}
+	
+	[Fact]
+	public async Task SendGetWithTypeInfo()
+	{
+		var result = await CreateFixture()
+			.AppendPathSegment("comments")
+			.AppendParameter("postId", 1)
+			.SendJsonAsync(HttpMethod.Get, null, PostCommentRecordContext.Default.PostCommentRecordArray)
+			.ToJsonStringAsync();
+		
+		await Verify(result);
+	}
+	
+	[Fact]
+	public async Task SendPostWithOptions()
+	{
+		var jsonOptions = new JsonSerializerOptions
+		{
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+		};
+		
+		var data = new PostCreateRecord
+		{
+			UserId = 2,
+			Title = "Japanese grammar",
+			Body = "Verbs, adjectives and nouns"
+		};
+		
+		var result = await CreateFixture()
+			.AppendPathSegment("posts")
+			.SendJsonAsync<PostCreateRecord>(HttpMethod.Post, data, jsonOptions)
+			.ToJsonStringAsync();
+		
+		await Verify(result);
+	}
+	
+	[Fact]
+	public async Task SendPostWithTypeInfo()
+	{
+		var data = new PostCreateRecord
+		{
+			UserId = 2,
+			Title = "Japanese grammar",
+			Body = "Verbs, adjectives and nouns"
+		};
+		
+		var result = await CreateFixture()
+			.AppendPathSegment("posts")
+			.SendJsonAsync(HttpMethod.Post, data, PostCreateRecordContext.Default.PostCreateRecord)
+			.ToJsonStringAsync();
+		
+		await Verify(result);
+	}
+}
---
+++ tests/MyNihongo.FluentHttp.Tests.Integration/Models/PostCommentRecord.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyNihongo.FluentHttp.Tests.Integration.Models;
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(PostCommentRecord[]))]
+internal partial class PostCommentRecordContext : JsonSerializerContext
+{
+}
+
+public sealed record PostCommentRecord
+{
+	public int PostId { get; set; }
+	
+	public int Id { get; set; }
+
+	public string Name { get; set; } = string.Empty;
+	
+	public string Email { get; set; } = string.Empty;
+	
+	public string Body { get; set; } = string.Empty;
+}
